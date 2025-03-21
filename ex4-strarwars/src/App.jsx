a0f093729c52bc0d4272af192e65c436e5371982
Ana Beatriz Romero <133364091+Anabia-05@users.noreply.github.com>
2025-03-21 11:33:29 +0000

    tentativa de juntar a api starwars

--- ex4-strarwars/src/App.jsx
+++ ex4-strarwars/src/App.jsx
@@ -1,12 +1,30 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+//import reactLogo from "./assets/react.svg";
+//import viteLogo from "/vite.svg";
 import "./App.css";
+import { Cabecalho } from "./components/Cabecalho";
 
+const urlStarWars = "https://swapi.dev/api/people/?page=1";
 function App() {
-  const [count, setCount] = useState(0);
+  const [personagens, setPersonagens] = useState([]);
 
-  return <></>;
+  async function carregar(){
+    try{
+      const response = await fetch(urlStarWars);
+      console.log("response",response);
+      const data= await response.json();
+      console.log("data",data)
+
+    }
+    catch (err){
+      console.log("err: ",err);
+    }
+  }
+
+  return <>
+  <button onClick={carregar}>  Carregar</button>
+  <Cabecalho/>
+  </>;
 }
 
 export default App;
