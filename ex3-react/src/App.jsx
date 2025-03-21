a85b08b20cd5995271173eb91d149c9c9b97c90c
Ana Beatriz Romero <133364091+Anabia-05@users.noreply.github.com>
2025-03-21 11:11:49 +0000

    aprendendo react

--- ex3-react/src/App.jsx
+++ ex3-react/src/App.jsx
@@ -1,38 +1,42 @@
-import { useState } from 'react'
+import { useState } from "react";
 //import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
 //import './App.css'
-import { Cabecalho } from './components/Cabecalho'
+import { Cabecalho } from "./components/Cabecalho";
 
 function App() {
   const [count, setCount] = useState(0);
   const [incremento, setIncremento] = useState(1);
-  
-  function handleBtIncrementarClick(){
+
+  function handleBtIncrementarClick() {
     setCount(count + 1);
   }
 
-  function handleBtLimparClick(){
+  function handleBtLimparClick() {
     setCount(0);
   }
 
-  function handleBtIncrementarChange(){
-    setIncremento(EventTarget.value);
+  function handleBtIncrementarValorClick() {
+    setContador(count + incremento);
   }
 
-  function handleBtIncrementarChange(){
-    setIncremento(EventTarget.value);
+  function handleBtIncrementarChange() {
+    ssetIncremento(parseInt(evt.target.value));
   }
   return (
     <>
-     <Cabecalho/> 
-     <p>Quantidade de cliques: {count}</p>
-     <button onClick ={handleBtIncrementarClick}> incrementar</button>
-     <button onClick ={handleBtLimparClick}> Limpar</button>
-     <input value={incremento} onChange={handleBtIncrementarChange}/>
-     <button onClick ={handleBtLimparClick}> Limpar</button> 
+      <Cabecalho />
+      <p>Quantidade de cliques: {count}</p>
+      <button onClick={handleBtIncrementarClick}> incrementar</button>
+      <button onClick={handleBtLimparClick}> Limpar</button>
+      <input
+        type="number"
+        value={incremento}
+        onChange={handleBtIncrementarChange}
+      />
+      <button onClick={handleBtIncrementarValorClick}>Incrementar Valor</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ ex4-strarwars/components/Cabecalho.jsx
@@ -0,0 +1,9 @@
+export function Cabecalho(){
+    return(
+        <div>
+            <h1>Programacao web mobile</h1>
+            <h2>Curso de Ciência da Computação - Unicap</h2>
+            <hr/>
+        </div>
+    );
+}
